75c8d3ee7631ce542fedac8a3be152c20c4177ee
discophile <45620991+discophile@users.noreply.github.com>
2018-12-07 09:40:05 +0800

    Update test.C

--- test.C
+++ test.C
@@ -1,6 +1,15 @@
 #include "cc/hdr/def.h"
+Test:test()
+{
+    print("create test\n");
+}
+Test:print()
+{
+    print("this is test\n");
+}
 void main()
 {
-    print "this is test\n"
-    return
+    Test Ins = new test();
+    Ins.print("this is main\n");
+    return;
 }
